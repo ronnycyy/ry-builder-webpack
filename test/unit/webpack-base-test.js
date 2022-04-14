c860513fdfbb07c0164d544e807d85e4cc0eee41
ronnycyy <m17722548276@163.com>
2022-04-15 00:24:25 +0800

    test: webpack.base test

--- test/unit/webpack-base-test.js
+++ test/unit/webpack-base-test.js
@@ -9,7 +9,8 @@ describe('webpack.base.js test case', () => {
   const baseConfig = require('../../lib/webpack.base.js');
 
   it('entry', () => {
-    assert.strictEqual(baseConfig.entry.index, '/Users/chenyunyi/Desktop/webpack/learn-webpack/builder-webpack/test/smoke/template/src/index/index.js');
-    assert.strictEqual(baseConfig.entry.search, '/Users/chenyunyi/Desktop/webpack/learn-webpack/builder-webpack/test/smoke/template/src/search/index.js');
+    // 如果在 travis 服务器上，路径无需全等，包含即可
+    assert.strictEqual(baseConfig.entry.index.indexOf('ry-builder-webpack/test/smoke/template/src/index/index.js') > -1, true);
+    assert.strictEqual(baseConfig.entry.search.indexOf('ry-builder-webpack/test/smoke/template/src/search/index.js') > -1, true);
   })
 })
